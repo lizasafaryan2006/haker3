ffcaa7d8704ec6ba9563db817f0e6ade66a30e1b
rafomn <rafayel.m11.y@tumo.org>
2023-02-12 12:43:55 +0400

    Lesson\ 2

---
+++ lesv/class.js
@@ -0,0 +1,99 @@
+class creature {
+    constructor(x, y, multiply) {
+        this.x = x;
+        this.y = y;
+        this.multiply = 0;
+}
+}
+
+class Grass extends creature {
+    super(x,y, multiply)
+    constructor() {
+    
+    this.directions = [
+          [this.x - 1, this.y - 1],
+          [this.x, this.y - 1],
+          [this.x + 1, this.y - 1],
+          [this.x - 1, this.y],
+          [this.x + 1, this.y],
+          [this.x - 1, this.y + 1],
+          [this.x, this.y + 1],
+          [this.x + 1, this.y + 1]
+      ]
+    }
+    
+    chooseCell(character) {
+        var found = []
+      for (var i in this.directions) {
+         var x = this.directions[i][0]
+         var y = this.directions[i][1]
+      if(x>=0 && x<matrix[0].length && y>=0 && y<matrix.length){
+      if (matrix[y][x] == character) {
+        found.push(this.directions[i])
+      }
+     }
+    
+    }
+     return found;
+    
+    }
+    mult() {
+       var empty = random(this.chooseCell(0))
+       this.multiply++
+     if (empty && this.multiply > 3) {
+       var newX = empty[0]
+       var newY = empty[1]
+        matrix[newY][newX] = 2
+       var newGr = new Grass(newX, newY)
+        grassArr.push(newGr)
+      }
+     }
+    }
+    class Xaxacox extends creature{
+        super(x,y, multiply)
+      constructor(energy) {
+          this.energy = 3;
+  
+      }
+      newDirections() {
+          this.directions = [
+              [this.x - 1, this.y - 1],
+              [this.x, this.y - 1],
+              [this.x + 1, this.y - 1],
+              [this.x - 1, this.y],
+              [this.x + 1, this.y],
+              [this.x - 1, this.y + 1],
+              [this.x, this.y + 1],
+              [this.x + 1, this.y + 1]
+          ];
+      }
+      getDirections(t) {
+          this.newDirections();
+          var found = [];
+  
+          for (let i = 0; i < this.directions.length; i++) {
+              var x = this.directions[i][0];
+              var y = this.directions[i][1];
+              if (x >= 0 && x < matrix[0].length && y >= 0 && y < matrix.length) {
+                  if (matrix[y][x] == t) {
+                      found.push(this.directions[i]);
+                  }
+              }
+          }
+          return found;
+      }
+      move(){
+           
+          var empty = random(this.getDirections(2))
+  
+          if (empty) {
+              var x = empty[0];
+              var y = empty[1];
+              matrix[y][x] = 3;
+              matrix[this.y][this.x] = 2;
+              this.x = x;
+              this.y = y;
+  
+          }
+      }
+    }
